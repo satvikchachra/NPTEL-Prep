e6a156ad83affdf7f57fe9cdd70c3f2bf9661c47
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2019-11-16 22:46:52 +0530

    Week 4

---
+++ q1 factorial.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 13:38:44 2019
+
+@author: Satvik Chachra
+"""
+
+n = int(input())
+product= 1
+while(n!=1):
+    product *= n;
+    n=n-1
+print(product)
