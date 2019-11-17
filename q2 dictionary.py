e16255c239fd094fff258adc67d38b4cb34d8be6
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2019-11-17 17:47:59 +0530

    Week 6

---
+++ q2 dictionary.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 20:11:00 2019
+
+@author: Satvik Chachra
+"""
+
+d = dict()
+n = int(input())
+for i in range(1,n+1):
+    d[i] = i*i*i
+print(d)
+    
